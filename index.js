8647db16c8ae81d1d716c12b2449ff83db4d40b3
Eli Moshe <moshebogo@gmail.com>
2023-01-15 14:05:39 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+
+
+
+
+
+
+function findMatching (drivers, string) {
+  return drivers.filter( driver => driver.toLowerCase() === string.toLowerCase())
+}
+
+
+function fuzzyMatch (drivers, firstLetters) {
+   return drivers.filter(letters => letters.slice(0, 2)  === firstLetters )
+}
+
+
+function matchName(drivers, string) {
+  return drivers.filter( driver => driver.name === string)
+}
